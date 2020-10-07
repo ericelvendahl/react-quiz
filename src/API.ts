9fec558720d6cedff3b5e547858349cd9107719a
“ericelvendahl” <ericelvendahl@gmail.com>
2020-10-07 15:58:53 -0500

    building scaffold for answer storage

--- src/API.ts
+++ src/API.ts
@@ -1,11 +1,30 @@
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answer: string[];
+  question: string;
+  type: string;
+};
+
+export type QuestionState = Question & { answers: string[] };
+
 export enum Difficulty {
-    EASY = "east",
-    MEDIUM = "medium",
-    HARD = "hard"
+  EASY = "east",
+  MEDIUM = "medium",
+  HARD = "hard",
 }
 
-export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
-    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficult=${difficulty}&type=multiple`;
-    const data = await (await fetch(endpoint)).json();
-    console.log(data);
-}
+export const fetchQuizQuestions = async (
+  amount: number,
+  difficulty: Difficulty
+) => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficult=${difficulty}&type=multiple`;
+  const data = await (await fetch(endpoint)).json();
+    return data.results.map((question: Question) => (
+    {
+        ...question
+        answer: 
+    }
+  )
+};
